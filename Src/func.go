ff31e4cb61ad534c43968700fe7ac0f65cb0dd0e
Administrator <65334265@qq.com>
2018-08-06 16:33:48 +0800

    2018/08/06

---
+++ Src/func.go
@@ -0,0 +1,23 @@
+package Src
+
+import "fmt"
+
+func eval(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		panic("unsupport op" + op)
+	}
+
+}
+
+func main() {
+	fmt.Println(eval(1, 2, "+"))
+}
