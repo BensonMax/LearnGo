74af0703b6c4dfeed7195b6c212d30effe7fbd61
Administrator <65334265@qq.com>
2018-08-11 18:22:00 +0800

    20180811

--- Src/Array.go
+++ Src/Array.go
@@ -13,9 +13,29 @@ func main() {
 	for i := range array3 {
 		fmt.Println(i)
 	}
+	//进阶 返回key value
+	for i, v := range array3 {
+		fmt.Println(i, v)
+	}
+	//进阶2 只返回value
+	for _, v := range array3 {
+		fmt.Println(v)
+	}
 
 	//遍历数组2
 	for i := 0; i < len(array3); i++ {
 		fmt.Println(i)
+	}
+
+	//求数组中最大的 key 和 value
+	numbers := [6]int{1, 3, 5, 7, 9, 11}
+
+	maxi := -1
+	maxValue := -1
+	for i, v := range numbers {
+		if v > maxValue {
+			maxi, maxValue = i, v
+		}
+		fmt.Println(maxi, maxValue)
 	}
 }
